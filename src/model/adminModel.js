2f0632c6da640b57d3dcaf5e5015b38113f2a428
muhammed-rasiq <razicodex@gmail.com>
2026-08-27 22:59:03 +0530

    added admin login shema

---
+++ src/model/adminModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const LoginSchema = new  mongoose.Schema({
+
+    AdminName:{
+        type:String,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+    
+})
+
+const admin = mongoose.model('admin', LoginSchema)
+
+export default admin;
